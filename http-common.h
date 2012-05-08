c8a71ff56c4f3aab0a7e40338f056666553bc16a
Advait Shinde <advait.shinde@gmail.com>
2012-05-07 17:06:36 -0700

    Basic socket accepting, reading, and writing!

---
+++ http-common.h
@@ -0,0 +1,9 @@
+/**
+ * http-common.h
+ * Defines common utilities used throughout the project
+ */
+
+#ifndef http_common_h
+#define http_common_h
+
+#endif
--- http-proxy.cc
+++ http-proxy.cc
@@ -1,12 +1,68 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright 2012 Advait Shinde
+ * CS118 - Event-based HTTP Proxy
+ */
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 using namespace std;
 
-int main (int argc, char *argv[])
-{
-  // command line parsing
+#define LISTENING_PORT 12345
+#define LISTEN_BACKLOG 5
+
+void error(const char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main (int argc, char *argv[]) {
+  // Setup listening socket
+  int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (!sock_fd) {
+    error("Unable to create listening socket");
+  }
+  
+  // Bind listening socket to address and port
+  sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(LISTENING_PORT);
+  if (bind(sock_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+    error("Unable to bind listening socket");
+  }
+  if (listen(sock_fd, LISTEN_BACKLOG) < 0) {
+    error("Unable to listen to socket fd");
+  }
+  printf("Listening on port %d\n", LISTENING_PORT);
+  
+  // Accept connection
+  sockaddr_in client_addr;
+  int client_fd;
+  socklen_t client_addr_len = sizeof(client_addr);
+  client_fd = accept(sock_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+  if (client_fd < 0) {
+    error("Unable to accept connection");
+  }
+  
+  // Read request
+  char buffer[256];
+  int n = read(client_fd, buffer, 256);
+  if (n < 0) {
+    error("Unable to read from socket");
+  }
+  
+  
+  // Write response
+  if (write(client_fd, "Got your message bro", 20) < 0) {
+    error("Unable to write to socket");
+  }
+  close(client_fd);
+  
   
   return 0;
 }
